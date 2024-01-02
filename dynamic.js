eb76e1ec7d34942ce63fe3d8eac400b7694b2ab5
Satya Dillikar <satya.dillikar@gmail.com>
2024-01-01 16:25:53 -0800

    price update

--- dynamic.js
+++ dynamic.js
@@ -7,7 +7,12 @@
     // Contact Detail end 
 
     // ABOUT US Detail start
-    var About_us_detail = "Welcome to our nurturing learning center, where your child's growth is our joy! With a team of dedicated educators, we craft personalized learning experiences in a safe and engaging environment. Whether it's game development, coding, or robotics, we focus on your child's unique needs and interests, ensuring a fun-filled educational journey.";
+    var About_us_detail = "Welcome to our nurturing learning center, where your \
+    child's growth is our joy! With a team of dedicated educators, we craft \
+    personalized learning experiences in a safe and engaging environment. \
+    Whether it's game development, coding, or robotics, we focus on your \
+    child's unique needs and interests, ensuring a fun-filled educational \
+    journey.";
     // ABOUT US Detail end
 
     // Why vGuru Title start
@@ -16,7 +21,11 @@
 
     // Card frist Start
     var Card_first_title = "Artficial Intelligence";
-    var Card_first_description = "Hey Brainiacs! AI, or Artificial Intelligence, is like a digital superhero! It's the tech magic that teaches computers to think, learn, and solve problems – just like you do! Imagine having a study buddy that's super smart and always ready for a brainy adventure. That's AI in action!<br> 📝🌟";
+    var Card_first_description = "Hey Brainiacs! AI, or Artificial Intelligence, \
+    is like a digital superhero! It's the tech magic that teaches computers to \
+    think, learn, and solve problems – just like you do! Imagine having a study \
+    buddy that's super smart and always ready for a brainy adventure. That's AI \
+    in action!<br> 📝🌟";
     // Card frist End 
 
     // Card Second Start
@@ -49,30 +58,44 @@
 
     // Card five Start
     var Card_five_title = "Robotics";
-    var Card_five_description = "Robotics is the ultimate fusion of tech and creativity!  It's like being a digital architect, designing and building smart machines. With robotics, you'll dive into coding, mechanics, and electronics, creating bots that move, groove, and solve real-world challenges. Get ready to engineer the future with a touch of fun! <br> 👩‍💻🔧";
+    var Card_five_description = "Robotics is the ultimate fusion of tech and \
+    creativity!  It's like being a digital architect, designing and building \
+    smart machines. With robotics, you'll dive into coding, mechanics, and \
+    electronics, creating bots that move, groove, and solve real-world \
+    challenges. Get ready to engineer the future with a touch of fun! <br> 👩‍💻🔧";
     // Card Five End
 
     // card six Start
     var Card_six_title = "Beginner Coding";
-    var Card_six_description = "Beginner coding is like embarking on a digital adventure! It's where you learn the language of computers in a fun and interactive way. Start with the basics, unravel the secrets of code, and soon you'll be crafting your own digital masterpieces. Join the coding journey – it's a blast! <br> ♟️💡 ";
+    var Card_six_description = "Beginner coding is like embarking on a digital \
+    adventure! It's where you learn the language of computers in a fun and \
+    interactive way. Start with the basics, unravel the secrets of code, \
+    and soon you'll be crafting your own digital masterpieces. Join the coding \
+    journey – it's a blast! <br> ♟️💡 ";
     // card six End
 
     // Enrollment Start
    
-    // Pre-school Detail Start
+    // coding classes
     var full_day_title = "Monday - Friday";
-    var full_day_time = "Hours: 5:00pm - 6:30pm, 6:30pm- 8:00pm";
-    var full_day_price = "Tuition: Call for pricing";
+    var full_day_time = "Hours: 5:00pm - 8:00pm";
+    var full_day_price = "Weekly 1 Hour Class - (4 classes) $149/ per month <br>\
+    OR &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp 2 Hour Per Week Membership  - $249/per month (Choose any two days)<br>";
     // Pre-school Detail End
    
-    // After School Detail Start
+    // coding camps
     var half_day_title = "Monday - Friday";
-    var half_day_time = "Full Day : 9:00am - 6:00pm, Half Day :9:00am - 12:00pm";
-    var half_day_price = "Tuition: Call for pricing";
+    var half_day_time = "Full Day : 9:00am - 4:00pm";
+    var half_day_price = "One day Camp $119/day <br> \
+    OR &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp Two day Camp $219/day <br>\
+    OR &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp Five day Camp $549/day <br>";
     // After School Detail End
 
     // terms and conditions Start
-    var terms_and_conditions = " <br> Non-refundable registration fee is applicable. <br> Tuition is paid monthly and is processed at the beginning of each month.";
+    var terms_and_conditions = " <br> <b>Try Us Risk-Free</b> <br> \
+    Start today, risk-free with our 4-session 100% money-back guarantee. <br> \
+    If your child doesn’t love it, then receive a full refund within the first 4 sessions. \
+    <br> Tuition is paid monthly and is processed at the beginning of each month.";
     // terms and conditions End
     // Enrollment End
 
